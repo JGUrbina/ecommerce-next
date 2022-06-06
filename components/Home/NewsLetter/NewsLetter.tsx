8208f87d7148e6d7a0437448995f5af075baae33
José Urbina <j.urbina.0183@gmail.com>
2022-06-06 19:26:47 -0300

    cambio de extención a al banner newsletter

--- components/Home/NewsLetter/NewsLetter.tsx
+++ components/Home/NewsLetter/NewsLetter.tsx
@@ -7,7 +7,7 @@ import NewsForm from '../../Global/NewsForm/NewsForm';
 
 const NewsLetter: React.FC = () => {
   return (
-    <Container image={'img/banner/b14.jpg'}>
+    <Container image={'img/banner/b14.png'}>
       <NewsText />
       <NewsForm />
     </Container>
